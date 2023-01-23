1848f5897290fbadb46d3a43d39c0d22d04f8ded
Shahin ABDI(IAS-1655) <shahin.abdi@ias.u-psud.fr>
2023-01-23 19:46:36 +0100

    ORM finished.

--- playground/views.py
+++ playground/views.py
@@ -2,8 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.db.models import Q, F
 from django.core.exceptions import ObjectDoesNotExist
-from store.models import Product, OrderItem, Order
-from django.db.models.aggregates import Count
+from store.models import Product, OrderItem, Order, Customer
 # Create your views here.
 
 
@@ -16,6 +15,10 @@ def index(request):
     return render(request, 'index.html',{'name': 'Django'})
 
 
+
+from django.db import transaction
+
+# @transaction.atomic()
 def orm_test(request):
     #query_set = Product.objects.all()
     # print(query_set[0:5])
@@ -90,8 +93,102 @@ def orm_test(request):
     #return render(request, 'index.html', {'name': 'Django', 'orders': list(query_set)})
 
     # Aggregate
-    from django.db.models.aggregates import Count, Min, Max, Avg, Sum
+    # from django.db.models.aggregates import Count, Min, Max, Avg, Sum
 
-    result = OrderItem.objects.filter(product__id=1).aggregate(units_sold=Sum('quantity'))
+    # result = OrderItem.objects.filter(product__id=1).aggregate(units_sold=Sum('quantity'))
     
-    return render(request, 'index.html', {'name': 'Django', 'result': result})
+    # return render(request, 'index.html', {'name': 'Django', 'result': result})
+    
+    #Annotating
+    # from django.db.models import Value, F, Func
+    # query_set = Customer.objects.annotate(is_new=Value(True))
+
+    # Calling Database Function
+    # query_set = Customer.objects.annotate(full_name = Func(F('first_name'), Value(' '),F('last_name'), function='CONCAT'))
+    # from django.db.models.functions import Concat
+    # query_set = Customer.objects.annotate(full_name = Concat(F('first_name'), Value(' '),F('last_name')))
+    # from django.db.models import Count
+
+    # #query_set = Customer.objects.annotate(orders_count=Count('order'))
+
+    # #Expression Wrapper
+    # #query_set = Product.objects.annotate(discount_price = F('unit_price') * 0.8) #Cannot infer type of '*' expression involving these types: DecimalField, FloatField. You must set output_field.
+    # # from django.db.models import ExpressionWrapper, DecimalField
+    # # discounted_price = ExpressionWrapper(F('unit_price') * 0.8, output_field=DecimalField())
+    # # query_set = Product.objects.annotate(discounted_price=discounted_price) 
+    # # return render(request, 'index.html', {'name': 'Django', 'result': list(query_set)})
+
+    # #Querying Generic Relationships
+    # from django.contrib.contenttypes.models import ContentType
+    # from store.models import Product
+    # from tags.models import TaggedItem
+
+    # # content_type = ContentType.objects.get_for_model(Product)
+    # # query_set = TaggedItem.objects.select_related('tag').filter(content_type=content_type, object_id=1)
+
+    # query_set = TaggedItem.objects.get_tags_for(Product, 1)  # Exact same as above but we just encapsulate it
+
+    # return render(request, 'index.html', {'name': 'Django', 'result': list(query_set)})
+
+    # #Cache
+    # query_set = Product.objects.all()
+    # list(query_set)
+    # query_set[0] #Use cache
+    # Use 3 Queries 
+
+    # query_set = Product.objects.all()
+    # query_set[0] #Don't Use cache, Use a query 
+    # list(query_set)
+    # Use 4 Queries 
+
+
+    #Insert INTO 
+
+    from store.models import Collection
+    # # Creating Objects
+    # collection = Collection()
+    # collection.title = 'Video Games'
+    # collection.featured_product = Product(pk=1)
+    # collection.save()
+
+
+    # Update 
+    # collection = Collection(pk=11)
+    # collection.title = 'Games'
+    # collection.featured_product = None
+    # collection.save()
+    
+    # If we dont set title here, we will have a problem so we have to first get pk then update field.
+ 
+    # collection = Collection.objects.get(pk=11)
+    # collection.featured_product = None
+    # collection.save()
+
+    # Collection.objects.filter(pk=11).update(featured_product=None)
+
+    # Delete 
+    # collection = Collection(pk=11)
+    # collection.delete() # All Collection
+
+    #Collection.objects.filter(id__gt=5).delete()
+    
+
+
+    #Transaction 
+
+    # with transaction.atomic():
+    #     order = Order()
+    #     order.customer_id = 1
+    #     order.save()
+
+
+    #     item = OrderItem()
+    #     item.order = order
+    #     item.product_id = 1
+    #     item.quantity = 1
+    #     item.unit_price = 10
+    #     item.save()
+
+    query_set = Product.objects.raw('SELECT * FROM store_product')
+
+    return render(request, 'index.html', {'name': 'Django', 'result': list(query_set)})
